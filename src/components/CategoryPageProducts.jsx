69d8c3ee3f3bce0998ffb547749201cba78aa9b4
Sean Patrick Wallace <wallace.sean111@gmail.com>
2023-04-28 16:00:59 +0200

    Restyle cat pages to use grid at >1024 breakpoint

--- src/components/CategoryPageProducts.jsx
+++ src/components/CategoryPageProducts.jsx
@@ -4,19 +4,21 @@ import Image from "next/image";
 import LinkButton from "@/components/LinkButton";
 
 const CategoryPageProducts = ({ products, slug }) => {
-  const productsJSX = products.map((product) => {
-    if (product.isNew) {
+  const productsJSX = products.map((product, index) => {
+    let isNew;
+    if (product.isNew) isNew = <p className={styles.newproduct}>New Product</p>;
+    if (index === 0 || index === 2) {
       return (
         <div key={uuid()} className={styles.product}>
-          <div className={styles.imgcontainer}>
+          <div className={styles.imgcontainerleft}>
             <Image
               src={product.preview}
               alt={product.name}
               className={styles.img}
             />
           </div>
-          <div className={styles.contentcontainer}>
-            <p className={styles.newproduct}>New Product</p>
+          <div className={styles.contentcontainerright}>
+            {isNew}
             <h2 className={styles.name}>{product.name}</h2>
             <p className={styles.description}>{product.description}</p>
             <LinkButton
@@ -26,20 +28,24 @@ const CategoryPageProducts = ({ products, slug }) => {
           </div>
         </div>
       );
-    }
+    };
     return (
       <div key={uuid()} className={styles.product}>
-        <div className={styles.imgcontainer}>
+        <div className={styles.imgcontainerright}>
           <Image
             src={product.preview}
             alt={product.name}
             className={styles.img}
           />
         </div>
-        <div className={styles.contentcontainer}>
+        <div className={styles.contentcontainerleft}>
+          {isNew}
           <h2 className={styles.name}>{product.name}</h2>
           <p className={styles.description}>{product.description}</p>
-          <LinkButton url={`/product/${product.slug}`} content="See Product" />
+          <LinkButton
+            url={`/product/${product.slug}`}
+            content="See Product"
+          />
         </div>
       </div>
     );
